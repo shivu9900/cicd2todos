8c6629f082e65269fb3cb9a1517b4faea397587d
Bala Krishna Ragala <balakrishn.ragala@upgrad.com>
2025-08-17 13:59:06 +0530

    jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest"
+  },
+  moduleFileExtensions: ["js", "jsx"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom"]
+};
